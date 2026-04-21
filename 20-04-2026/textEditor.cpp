e1e19577de7130811fa8bc7b03a9d53ada71f403
Karlen <karlen.matevosyan.ec@gmail.com>
2026-04-21 17:52:27 +0400

    Updated edge cases with undo and delete

--- 20-04-2026/textEditor.cpp
+++ 20-04-2026/textEditor.cpp
@@ -3,6 +3,17 @@
 #include <string>
 using namespace std;
 
+bool isDigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+bool isInt(string s) {
+    for (char c : s) {
+        if (!isdigit(c)) return false;
+    }
+    return true;    
+}
+
 int main() {
     string text = "";
     stack<string> history;
@@ -25,9 +36,22 @@ int main() {
             cout << "Enter 'type', 'delete', 'print', or 'undo': ";
         }
         else if (command == "delete") {
-            int k;
+            string kstr;
             cout << "Enter number of char to delete: ";
-            cin >> k;
+            cin >> kstr;
+            
+            if (!isInt(kstr)) {
+                cout << "Invalid k type value..." << endl;
+                cout << "Enter 'type', 'delete', 'print', or 'undo': ";
+                continue;
+            }
+            
+            int k = stoi(kstr);
+            if (k < 0) {
+                cout << "Invalid k value..." << endl;
+                cout << "Enter 'type', 'delete', 'print', or 'undo': ";
+                continue;
+            }
 
             history.push(text); 
 
@@ -42,8 +66,14 @@ int main() {
             cout << "Enter 'type', 'delete', 'print', or 'undo': ";
         }
         else if (command == "undo") {
-            text = history.top();
-            history.pop();
+            if (!text.empty()) {
+                text = history.top();
+                history.pop();
+            } else {
+                cout << "You can not type 'undo' at first... " << endl;
+                cout << "Enter 'type', 'delete', 'print', or 'undo': ";
+                continue;
+            }
             cout << "Enter 'type', 'delete', 'print', or 'undo': ";
         }
     }
